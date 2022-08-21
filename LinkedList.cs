089c92f921d6e3ec887f623c025ea874e411d42b
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:37:15 +0530

    create Linked List by appending 30 and 70

---
+++ LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        Node head;
+        Node current;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                //head is asigned to new node
+                node.next = this.head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                //assigning node to temp->next
+                temp.next = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace EmployeeWage
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Program");
+            LinkedList list = new LinkedList();
+
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+
+            list.Display();
+
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+
+            list.Display();
         }
     }
 }
