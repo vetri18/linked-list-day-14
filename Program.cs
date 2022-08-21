0bb12efab9b294d38ea03c005354c6837f47a70f
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:34:43 +0530

    create a  simple Linked

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace EmployeeWage
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Program");
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach (int ele in list)
+            {
+                Console.WriteLine(ele);
+            }
         }
     }
 }
