d407fbbc08489559f5ca99357f17977315f58e71
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:38:40 +0530

    insert 30 between 56  and 70

---
+++ LinkedList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        Node head;
+        Node current;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                //head is asigned to new node
+                node.next = this.head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                //assigning node to temp->next
+                temp.next = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+
+        public void Insert(int data, int posVal)
+        {
+            Node node = new Node(data);
+            Node temp = this.head;
+
+            while (temp != null)
+            {
+                if (temp.data == 56)
+                {
+                    //next of 50 assigned to new node
+                    //new node is assigning to 50 
+                    node.next = temp.next;
+                    temp.next = node;
+                    Console.WriteLine("New data is inserted in between 56 and 70");
+                    break;
+
+                }
+                temp = temp.next;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace EmployeeWage
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Program");
+            LinkedList list = new LinkedList();
+
+            list.Append(56);
+            list.Append(70);
+            list.Display();
+            list.Insert(30, 56);
+            list.Display();
         }
     }
 }
